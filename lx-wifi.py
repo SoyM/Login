d3e2ed6d45040fd983c091815ca9ea1579f24d09
SoyM <huangxinjiang1996@gmail.com>
2015-09-20 19:15:48 +0800

    Update notes
    Update  notes to use it easily.

--- lx-wifi.py
+++ lx-wifi.py
@@ -8,9 +8,9 @@ import cookielib
 
 
 hosturl = 'http://sj.dglongxi.com:88/Weixin.aspx?nasIp=113.98.10.144&nasPortId=DG-WJ-BAS-3.-43001290200000@vlan&ip=119.141.206.60&mac='
-
+#vlan&ip=119.141.206.60 , change119.141.60.119 to you ip address
 posturl = 'http://sj.dglongxi.com:88/Weixin.aspx?nasIp=113.98.10.144&nasPortId=DG-WJ-BAS-3.-43001290200000%vlan&ip=119.141.206.60&mac='
-
+#vlan&ip=119.141.206.60 , change119.141.60.119 to you ip address
 
 cj = cookielib.LWPCookieJar()
 cookie_support = urllib2.HTTPCookieProcessor(cj)
@@ -26,7 +26,7 @@ headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20
 
 
 postData = '__VIEWSTATE=%2FwEPDwULLTE3MTc4Nzg1MTVkZCq9h%2BftRRY%2Fu04HpKFIVPCw314wEn0AtpMtlrxX2jVP&__VIEWSTATEGENERATOR=79323071&__EVENTVALIDATION=%2FwEWAwLwwNWQCAK2xpONDwKBk7XADC4BQdj5QRNnVO46EHLJd%2FkvExw%2FAgGgV8RYixzJe6wH&tb_pw=02468&btn_ok=%E8%BF%9E%E6%8E%A5%E4%B8%8A%E7%BD%91'
-
+#tb_pw=02468, 02468 is the key what gets from wechat.The key changes once every ten days.
 
 request = urllib2.Request(posturl, postData, headers)
 print request
