c96678ddf2cf02498e87119770212524a3861d23
SoyM <huangxinjiang1996@gmail.com>
2015-09-23 12:51:51 +0800

    Get localIP automatically
    Now you don't have to type you ip address to run it.It becomes more
    intelligent.

--- lx-wifi.py
+++ lx-wifi.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 
-
 import urllib2
 import cookielib
+import socket
 
-
-
+localip = socket .gethostbyname(socket .gethostname() )
 hosturl = 'http://sj.dglongxi.com:88/Weixin.aspx?nasIp=113.98.10.144' \
-          '&nasPortId=DG-WJ-BAS-3.-43001290200000%vlan&ip=119.144.116.201&mac='
-
+          '&nasPortId=DG-WJ-BAS-3.-43001290200000%vlan&ip='
+hosturl = hosturl + localip + '&mac='
 
 cj = cookielib.LWPCookieJar()
 cookie_support = urllib2.HTTPCookieProcessor(cj)
@@ -17,10 +16,12 @@ urllib2.install_opener(opener)
 
 
 h = urllib2.urlopen(hosturl)
-
+hosturl = h.geturl()
+print localip
+print hosturl
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20100101 Firefox/14.0.1',
-           'Referer' : 'http://sj.dglongxi.com:88/Weixin.aspx?nasIp=113.98.10.144&nasPortId=DG-WJ-BAS-3.-43001290200000@vlan&ip=119.141.185.106&mac='}
+           'Referer' : 'http://sj.dglongxi.com:88/?nasIp=113.98.10.144&nasPortId=DG-WJ-BAS-3.-43001290200000@vlan'}
 
 
 postData = '__VIEWSTATE=%2FwEPDwULLTE3MTc4Nzg1MTVkZCq9h%2BftRRY%2Fu04HpKFIVPCw314wEn0AtpMtlrxX2jVP&__VIEWSTATEGENERATOR=79323071&__EVENTVALIDATION=%2FwEWAwLwwNWQCAK2xpONDwKBk7XADC4BQdj5QRNnVO46EHLJd%2FkvExw%2FAgGgV8RYixzJe6wH&tb_pw=02468&btn_ok=%E8%BF%9E%E6%8E%A5%E4%B8%8A%E7%BD%91'
